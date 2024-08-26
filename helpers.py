431b964ddc8cb830ad9bd2f415da79135e953507
JPW03 <03.jamie.w@googlemail.com>
2024-08-26 18:59:47 +0100

    Deciding which servers to buy

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 from enum import Enum, auto
 from typing import TypedDict
 import numpy as np
+import numpy.typing as npt
 from utils import load_problem_data
 
 
@@ -8,7 +9,7 @@ demand, datacenters, servers, selling_prices = load_problem_data()
 
 # Takes a timestep and returns a list of server types that are available for purcahse 
 # at that time step
-def get_available_servers(timestep: int) -> list[str]:
+def get_available_servers(timestep: int) -> tuple[list[str], list[str]]:
     available_servers: list[str] = []
 
     def check_server_availability(server: np.array):
@@ -17,8 +18,10 @@ def get_available_servers(timestep: int) -> list[str]:
             available_servers.append(server[0])
 
     servers.apply(check_server_availability, axis=1, raw=True)
-    return available_servers
 
+    available_cpu_servers = [s for s in available_servers if 'CPU' in s]
+    available_gpu_servers = [s for s in available_servers if 'GPU' in s]
+    return available_cpu_servers, available_gpu_servers
 
 # Dictionary that matches the structure of the actions in solution_example.json
 class Action(TypedDict):
@@ -27,3 +30,22 @@ class Action(TypedDict):
     server_generation: str
     server_id: str
     action: str
+
+# The number of days before the server breaks even (in DC1)
+# See the Profitability spreadsheet in the google drive
+server_profitability = {
+    'CPU.S1': 45,
+    'CPU.S2': 35,
+    'CPU.S3': 22,
+    'CPU.S4': 16,
+    'GPU.S1': 13,
+    'GPU.S2': 14,
+    'GPU.S3': 12
+}
+
+def get_most_profitable(servers: list[str]) -> str: 
+    return servers[np.argmin([server_profitability[s] for s in servers])]
+
+indexed_servers = servers.set_index('server_generation')
+def get_server_capacity(server_generation: str) -> int:
+    return indexed_servers.loc[server_generation, 'capacity']
--- mysolution.py
+++ mysolution.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pandas as pd
-from helpers import Action
+from helpers import Action, get_available_servers, get_most_profitable, get_server_capacity
 from seeds import known_seeds
 from utils import load_problem_data, save_solution
 from evaluation import get_actual_demand, get_known
@@ -16,8 +16,19 @@ def get_my_solution(actual_demand) -> list[Action]:
     
     for t in np.range(1,get_known('time_steps')):
         # Checks the demand that needs to be satisfied
+        unsatisfied_demand = get_unsatisfied_demand()
         
         # Buys servers if there is unsatisfied demand
+        ## which servers?
+        available_cpus, available_gpus = get_available_servers(t)
+        cpu_target = get_most_profitable(available_cpus)
+        gpu_target = get_most_profitable(available_gpus)
+
+        ## how many?
+        cpu_capacity = get_server_capacity(cpu_target)
+        gpu_capacity = get_server_capacity(gpu_target)
+        
+
         pass
 
 
