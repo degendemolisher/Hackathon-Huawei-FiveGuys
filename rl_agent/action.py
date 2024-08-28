13e1e9d08f95ec322f1200180890cb651e745b2d
Kwun <tsangkwunhei@gmail.com>
2024-08-29 02:30:18 +0800

    e

--- rl_agent/action.py
+++ rl_agent/action.py
@@ -1,5 +1,5 @@
 import numpy as np
-from collections import deque
+from collections import deque, defaultdict
 
 class ActionSpace:
     def __init__(self):
@@ -9,6 +9,8 @@ class ActionSpace:
         self.data_centers = ['DC1', 'DC2', 'DC3', 'DC4']
         self.server_id_counters = {gen: 0 for gen in self.server_generations}
         self.existing_servers = {dc: {gen: deque() for gen in self.server_generations} for dc in self.data_centers}
+         
+        self.Fleet = {dc: {gen: {'servers': [], 'total_owned': 0} for gen in self.server_generations} for dc in self.data_centers}
 
     def create_action(self, operation, server_generation, server_id, data_center):
         """
@@ -24,10 +26,25 @@ class ActionSpace:
         self.server_id_counters[server_generation] += 1
         return f"{server_generation}_{self.server_id_counters[server_generation]}"
 
-    def convert_actionspace_to_action(self, action_space):
+    def update_fleet(self, data_center, server_generation, server_id, operation, timestep):
+        """
+        Update the fleet record when a server is bought or dismissed.
+        """
+        if operation == 'buy':
+            self.Fleet[data_center][server_generation]['servers'].append({'server_id': server_id, 'timestep_bought': timestep})
+            self.Fleet[data_center][server_generation]['total_owned'] += 1
+        elif operation == 'dismiss':
+            if self.Fleet[data_center][server_generation]['total_owned'] > 0:
+                self.Fleet[data_center][server_generation]['servers'] = [
+                    s for s in self.Fleet[data_center][server_generation]['servers'] if s['server_id'] != server_id
+                ]
+                self.Fleet[data_center][server_generation]['total_owned'] -= 1
+
+    def convert_actionspace_to_action(self, action_space, timestep):
         """
         Convert the action space (defined as Box) into meaningful actions.
         :param action_space: The action space array from the RL environment.
+        :param timestep: Current timestep in the simulation.
         :return: The corresponding action tuples.
         """
         actions = []
@@ -47,6 +64,7 @@ class ActionSpace:
                                 self.existing_servers[data_center][server_generation].append(server_id)
                                 action = self.create_action(operation, server_generation, server_id, data_center)
                                 actions.append(action)
+                                self.update_fleet(data_center, server_generation, server_id, operation, timestep)
                         
                         elif operation == 'dismiss':
                             for _ in range(quantity):
@@ -55,6 +73,7 @@ class ActionSpace:
                                     server_id = self.existing_servers[data_center][server_generation].popleft()
                                     action = self.create_action(operation, server_generation, server_id, data_center)
                                     actions.append(action)
+                                    self.update_fleet(data_center, server_generation, server_id, operation, timestep)
                                 else:
                                     break   
 
@@ -79,10 +98,15 @@ class ActionSpace:
 if __name__ == "__main__":
     action_space = ActionSpace()
     
-    # Define the action space with shape (4, 7, 4, 1)
     rl_action_space = np.random.randint(0, 10001, size=(4, 7, 4, 1))
+    timestep = 1
     
-    actions = action_space.convert_actionspace_to_action(rl_action_space)
+    actions = action_space.convert_actionspace_to_action(rl_action_space, timestep)
     
     for action in actions:
         print(action)
+     
+    print("\nFleet Status:")
+    for dc in action_space.Fleet:
+        for sg in action_space.Fleet[dc]:
+            print(f"Datacenter: {dc}, Server Generation: {sg}, Fleet: {action_space.Fleet[dc][sg]}")
